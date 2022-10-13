a1fec5392a2bae7c0123687f4e944411f92f0276
atahhabibi <habibiatah@gmail.com>
2022-10-13 14:37:50 -0700

    finish birthday remainder project

--- 01-Birthday Remainder/src/App.js
+++ 01-Birthday Remainder/src/App.js
@@ -2,7 +2,23 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  const [people, setpeople] = useState(data);
+
+  const removeAll=()=>{
+   setpeople([]);
+  }
+
+  return <main>
+    <section className="container">
+
+    <h3>{people.length} birthday today</h3>
+    <List people={people}/>
+    <button onClick={removeAll}>clear All</button>
+
+    </section>
+
+  </main>;
 }
 
 export default App;
