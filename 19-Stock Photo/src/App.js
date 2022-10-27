8b420fb85340e13f46463a4f756de8ebaf634cea
atahhabibi <habibiatah@gmail.com>
2022-10-27 11:54:12 -0700

    completing Stock Photo External API

--- 19-Stock Photo/src/App.js
+++ 19-Stock Photo/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect,useRef } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
 
@@ -8,71 +8,150 @@ const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 
-//api access key
-
-/* ahMqdMokp9Y7CtlRGo7S7jgf53eedbzjAYel88aOLjw */
-
-//end root or base url 
-
-/* https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY */
-
-//full api with access code
-
-//https://api.unsplash.com/photos/?client_id=ahMqdMokp9Y7CtlRGo7S7jgf53eedbzjAYel88aOLjw
-
-
 
 function App() {
   
   const [loading,setLoading]=useState(false);
   const[photos,setPhotos]=useState([]);
-
+  const[page,setPage]=useState(1);
+  const[query,setQuery]=useState('');
+  const mounted = useRef(false);
+  const[newImages,setNewImages]=useState(false);
+  
 
   const fetImages=async()=>{
 
     setLoading(true);
 
     let url;
+    const urlPage=`&page=${page}`
+    const urlQuery=`&query=${query}`;
 
-    url=`${mainUrl}${clientID}`;
+    
+    if(query){
+      url=`${searchUrl}${clientID}${urlPage}${urlQuery}`
+
+    }else{
+      
+      url=`${mainUrl}${clientID}${urlPage}`;
+    }
+
+
 
     try {
 
       const response=await fetch(url);
       const data=await response.json();
-      console.log(data)
+     
+      setPhotos((oldPhotos)=>{
+
+        if(query && page===1){
+
+          return data.results;
+
+        }else if(query){
+          return [...oldPhotos,...data.results]
+        }else return [...oldPhotos,...data]
+      })
+
+      setNewImages(false);
       setLoading(false)
-      
       
     } catch (error) {
       console.log(error);
+      setNewImages(false);
       setLoading(false);
       
     }
 
-
-
-
   }
-
+  
   useEffect(()=>{
     fetImages();
+    //eslint-disable-next-line
+  },[page])
+
+
+  useEffect(()=>{
+    if(!mounted.current){
+       mounted.current=true;
+       return ;
+    }
+
+    if(!newImages) return ; 
+    if(loading) return ; 
+
+
+     setPage((oldpage)=>{
+          return oldpage+1;
+        })
+    
+   
+  },[newImages])
+
+
+  const event=()=>{
+
+    if((window.innerHeight+window.scrollY)>=(document.body.scrollHeight-2)){
+      setNewImages(true);
+
+      }
+  }
+
+
+
+  useEffect(()=>{
+
+    window.addEventListener('scroll',event);
+    return ()=>window.removeEventListener('scroll',event)
+
   },[])
 
 
 
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    if(!query){
+      return ; 
+    }
+
+    if(page===1){
+     fetImages();
+     return ; 
+    }
+
+    setPage(1);
+  }
+
+  return <main>
+
+    <section className="search">
+
+      <form  className="search-form">
+        <input type="text" placeholder='search' className='form-input' value={query} onChange={(e)=>setQuery(e.target.value)}/>
+        <button type='submit' className='submit-btn' onClick={handleSubmit}><FaSearch/></button>
+      </form>
+
+    </section>
+
+    <section className="photos">
+      <div className="photos-center">
+
+        {photos.map((photo)=>{
+          return <Photo key={photo.id} photo={photo}/>
+        })}
+
+      </div>
+
+
+      {loading && <h2 className='loading'>Loading...</h2>}
 
 
 
+    </section>
 
-
-
-
-
-
-
-
-  return <h2>stock photos starter</h2>
+  </main>
 }
 
 export default App
