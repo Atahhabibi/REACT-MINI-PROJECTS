28e767d2681f52d19a4f36694502878bf4bb869d
atahhabibi <habibiatah@gmail.com>
2022-10-31 09:12:32 -0700

    refactring to react router 6

--- 21-Movie DB/src/App.js
+++ 21-Movie DB/src/App.js
@@ -4,16 +4,22 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
 import Movie from './SingleMovie'
 import Error from './Error'
+import {Routes} from 'react-router-dom'
+import SingleMovie from './SingleMovie'
 
 function App() {
-  return <Switch>
+  return <Routes>
 
-    <Route path='/' exact><Home/></Route>
+    <Route path='/' element={<Home/>}/>          
+    <Route path='/movies/:id' element={<SingleMovie/>}/>
+    <Route path='*' element={<Error/>}/>
+
+    {/* <Route path='/' exact><Home/></Route>       // Reacter router 5 
     <Route path='/movies/:id' children={<Movie/>}/>
-    <Route path='*'><Error/></Route>
+    <Route path='*'><Error/></Route> */}
 
 
-  </Switch>
+  </Routes>
 }
 
 export default App
--- 21-Movie DB/src/index.js
+++ 21-Movie DB/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { AppProvider } from './context'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
-      <Router>
+      <BrowserRouter>
 
       <App />
 
-      </Router>
+      </BrowserRouter>
     </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
